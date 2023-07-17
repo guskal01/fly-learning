ddb8a9e06f37037d827ef8faf39bfd7c08877506
axelamit <axel.v.nilsson@gmail.com>
2023-07-17 22:08:47 +0000

    only linear layers and add isolationforest

--- defences/pca_defense.py
+++ defences/pca_defense.py
@@ -1,5 +1,25 @@
 from sklearn.decomposition import PCA
 import numpy as np
+from sklearn.ensemble import IsolationForest
+import matplotlib.pyplot as plt 
+
+def state_dict_to_vec_linear(state_dict):
+    weight_keys = [
+        "model.classifier.0.weight",
+        "model.classifier.3.0.weight",
+        "model.classifier.3.2.weight",
+        "model.classifier.3.4.weight"
+    ]
+    
+    bias_keys = [
+        "model.classifier.0.bias",
+        "model.classifier.3.0.bias",
+        "model.classifier.3.2.bias",
+        "model.classifier.3.4.bias"
+    ]
+
+    
+    return torch.cat([torch.flatten(state_dict[key]) for key in state_dict if key in weight_keys+bias_keys])
 
 class PCADefense():
     def __init__(self, dataloader):
@@ -11,15 +31,39 @@ class PCADefense():
 
         client_params = []
         for i in range(len(client_nets)): 
-            client_params.append([client_nets[i][key].cpu() for key in state_dict][0])
+            #print(state_dict_to_vec(client_nets[i]).detach().cpu())
+            client_params.append(list(state_dict_to_vec_linear(client_nets[i]).detach().cpu()))
 
         pca = PCA(n_components=2)
-        principal_components = pca.fit(client_params)
+        pca.fit(client_params)
+        x = pca.transform(client_params)
 
-        print(principal_components)
+        clf = IsolationForest(n_estimators=10, warm_start=True)
+        clf.fit(x) 
+        outliers = clf.predict(x) 
+
+        print(x.shape)
+        print(x)
+        print(outliers)
+
+        plt.scatter([p[0] for p in x], [p[1] for p in x])
+        plt.savefig("out.png")
+        assert 1==0
 
         for key in state_dict:
             state_dict[key] = sum([x[key] for x in client_nets]) / len(client_nets)
         
         net.load_state_dict(state_dict)
         return net
+
+
+if __name__ == "__main__":
+    from models import Net
+    from constants import *
+
+    net = Net().to(device)
+    clients = [Net().to(device).state_dict() for _ in range(10)]
+    selected = None 
+
+    defense = PCADefense(None)
+    defense.aggregate(net, clients, selected)
