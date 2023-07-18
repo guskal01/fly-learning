e6bbcf6031416a3643592f7ed26ca6d31e1457f9
axelamit <axel.v.nilsson@gmail.com>
2023-07-18 15:52:16 +0000

    init feature_importance

---
+++ defences/feature_importance.py
@@ -0,0 +1,17 @@
+
+class FeatureImportance():
+    def __init__(self, dataloader):
+        self.dataloader = dataloader
+    
+    def aggregate(self, net, client_nets, selected=None):
+        state_dict = net.state_dict()
+        
+        for key in state_dict:
+            state_dict[key] = sum([x[key] for x in client_nets]) / len(client_nets)
+        
+        net.load_state_dict(state_dict)
+        return net
+
+
+if __name__ == "__main__":
+    pass
