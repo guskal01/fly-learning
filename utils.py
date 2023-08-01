e4821164b3ecfa672ab039c1a92d2b3448048564
Gustav Kalander <guskal@chalmers.se>
2023-08-01 05:00:53 +0000

    Revert "cleaned fltrust code"
    This reverts commit f24ec95b0b97b0b99e7b9863a087bcd29935f543.

--- utils.py
+++ utils.py
@@ -3,7 +3,11 @@ from models import Net
 import copy
 
 def state_dict_to_vec(state_dict):
-    return torch.cat([torch.flatten(x) for key,x in state_dict.items() if 'num_batches_tracked' not in key])
+    for key in state_dict:
+        if 'num_batches_tracked' in key:
+            nbt = state_dict[key]
+            break
+    return torch.cat([torch.flatten(x) for key,x in state_dict.items() if 'num_batches_tracked' not in key]), nbt
 
 def vec_to_state_dict(vec, state_dict, nbt):
     i = 0
@@ -32,7 +36,8 @@ def weighted_avg(net, client_nets, weights):
     state_dict = net.state_dict()
         
     for key in state_dict:
-        state_dict[key] = sum([x[key]*weights[i] for i, x in enumerate(client_nets)])/sum(weights)
+        state_dict[key] = sum([x[key]*weights[i] for i, x in enumerate(client_nets)])
     
     net.load_state_dict(state_dict)
-    return net
+    return net, None
+
