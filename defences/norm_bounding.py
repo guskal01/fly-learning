34f32904b2622964add41e7bd35d4757967ea3ef
axelamit <axel.v.nilsson@gmail.com>
2023-08-02 20:05:11 +0000

    fix bug and change tau

--- defences/norm_bounding.py
+++ defences/norm_bounding.py
@@ -6,19 +6,21 @@ class NormBounding():
     def __init__(self, dataloader):
         self.dataloader = dataloader
         self.aggregator = FedAvg(dataloader)
-        self.tau = 0.001
+        self.tau =  1.0
         self.max_norm = 0
     
     def aggregate(self, net, client_nets, selected):
         state_dict = net.state_dict()        
         for key in state_dict:
-            if ("bias" in key or "weight" in key):
+            if 'num_batches_tracked' in key:
+                state_dict[key] = client_nets[0][key]
+            else:
                 for i in range(len(client_nets)):
                     client_nets[i] = self.bound_client_gradient(net, client_nets[i], key)
 
-        net = self.aggregator.aggregate(net, client_nets)
-
-        state_dict = net.state_dict()
+        net.load_state_dict(state_dict)
+        net, _ = self.aggregator.aggregate(net, client_nets)
+        net.load_state_dict(state_dict)
         return net, None
 
     def bound_client_gradient(self, net, client_net, key):
@@ -29,9 +31,10 @@ class NormBounding():
 
         if (norm > self.max_norm):
             self.max_norm = norm
+            print(norm)
 
         if (norm > self.tau):
-            client_net[key] *= self.tau/norm
+            client_net[key] = state_dict[key] + weight_gradient * self.tau/norm
         
         return client_net
     
