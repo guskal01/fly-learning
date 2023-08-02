e2a76efb1caaae9cd873054c057dee3e475f095d
axelamit <axel.v.nilsson@gmail.com>
2023-08-02 20:04:14 +0000

    trim all layers and fix bug

--- defences/trimmed_mean.py
+++ defences/trimmed_mean.py
@@ -5,19 +5,18 @@ import copy
 class TrimmedMean():
     def __init__(self, dataloader):
         self.dataloader = dataloader
-        self.k = 1
+        self.k = 2
     
     def aggregate(self, net, client_nets, selected):
         state_dict = net.state_dict()        
         for key in state_dict:
-            if ("bias" in key or "weight" in key):
-                state_dict[key] = self.trim_mean(client_nets, key)
+            state_dict[key] = self.trim_mean(client_nets, key)
 
-        state_dict = net.state_dict()
+        net.load_state_dict(state_dict)
         return net, None
 
     def trim_mean(self, client_nets, key):
-        client_params = torch.from_numpy(np.array([client_nets[i][key].cpu().numpy() for i in range(len(client_nets))]))
+        client_params = torch.from_numpy(np.array([client_nets[i][key].cpu().numpy().flatten() for i in range(len(client_nets))]))
         sorted_params = client_params.sort(dim=0)[0]
         trimmed_params = sorted_params[self.k:-self.k]
-        return trimmed_params.mean(dim=0)
+        return trimmed_params.float().mean(dim=0).reshape(client_nets[0][key].shape)
